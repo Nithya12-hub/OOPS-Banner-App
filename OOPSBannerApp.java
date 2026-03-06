719d93e8cd8524fbe066e5dcaeede39447f19f54
Nithya12-hub <nithshree12@gmail.com>
2026-03-06 14:36:02 +0530

    uc1 completed

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print the text "OOPS" to the console
+ * This is the first step of the OOPS Banner App project.
+ *
+ * @author Nithya
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - entry point of the Java program
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print the banner text
+        System.out.println("OOPS");
+
+    }
+}
