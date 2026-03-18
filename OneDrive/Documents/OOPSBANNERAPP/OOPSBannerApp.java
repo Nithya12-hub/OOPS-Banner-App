736330687fc9f50a9cb5102728d35db9e0b9c216
Nithya12-hub <nithshree12@gmail.com>
2026-03-18 08:54:02 +0530

    Completed all the practice problems

---
+++ OneDrive/Documents/OOPSBANNERAPP/OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp
+ * UC1: Printing the literal text "OOPS"
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Output the literal string to the console
+        System.out.println("OOPS");
+    }
+}
